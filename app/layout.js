b26d76d0bf0e85118b2446d15b77d03dd637db95
goutam2004 <goutamdawar68@gmail.com>
2026-08-23 23:35:03 +0530

    Add Google Analytics tracking

--- app/layout.js
+++ app/layout.js
@@ -90,7 +90,11 @@ export default function RootLayout({ children }) {
             >
               {`
                 window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
+
+                function gtag() {
+                  dataLayer.push(arguments);
+                }
+
                 window.gtag = gtag;
 
                 gtag('js', new Date());
